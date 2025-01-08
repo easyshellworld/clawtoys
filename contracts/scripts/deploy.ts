1cdb56732671796c3428ca6159b97c5220ae2146
shellcodewsl <shellcodewsl@exapmle.com>
2025-01-08 22:16:54 +0800

    frontend

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,33 +1,62 @@
-import { Provider, Contract, Account, ec, json } from 'starknet';
-import fs from 'fs';
-import dotenv from 'dotenv';
-
-// 加载环境变量
+import { Account, CallData, Contract, RpcProvider, stark } from "starknet";
+import * as dotenv from "dotenv";
+import { getCompiledCode } from "./utils";
 dotenv.config();
 
-// 配置 StarkNet 的 Provider
-const provider = new Provider({ network: 'goerli-alpha' });
-
-// 从环境变量中读取私钥和账户地址
-const privateKey = process.env.PRIVATE_KEY || '';
-const accountAddress = process.env.ACCOUNT_ADDRESS || '';
-
-// 创建账户对象
-const account = new Account(provider, accountAddress, privateKey);
-
-// 部署合约
-async function deployContract() {
-  // 读取编译后的合约文件
-  const compiledContract = json.parse(
-    fs.readFileSync('artifacts/MyContract.json').toString('ascii')
-  );
-
-  // 部署合约
-  const deployResponse = await account.deployContract({
-    contract: compiledContract,
+async function main() {
+  const provider = new RpcProvider({
+    nodeUrl: process.env.RPC_ENDPOINT,
   });
 
-  console.log('Contract deployed at:', deployResponse.contract_address);
-}
+  // initialize existing predeployed account 0
+  console.log("ACCOUNT_ADDRESS=", process.env.DEPLOYER_ADDRESS);
+  console.log("ACCOUNT_PRIVATE_KEY=", process.env.DEPLOYER_PRIVATE_KEY);
+  const privateKey0 = process.env.DEPLOYER_PRIVATE_KEY ?? "";
+  const accountAddress0: string = process.env.DEPLOYER_ADDRESS ?? "";
+  const account0 = new Account(provider, accountAddress0, privateKey0);
+  console.log("Account connected.\n");
+/*   const balance = await account0.getbalanceOf(accountAddress0);
+  console.log(`STRK Balance for ${accountAddress0}:`, balance.toString()); */
 
-deployContract();
+  // Declare & deploy contract
+  let sierraCode, casmCode;
+
+  try {
+    ({ sierraCode, casmCode } = await getCompiledCode(
+      "workshop_counter_contract"
+    ));
+  } catch (error: any) {
+    console.log("Failed to read contract files");
+    process.exit(1);
+  }
+
+  const myCallData = new CallData(sierraCode.abi);
+  const constructor = myCallData.compile("constructor", {
+    initial_value: 100,
+    kill_switch_address:
+      "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
+    initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
+  });
+  const deployResponse = await account0.declareAndDeploy({
+    contract: sierraCode,
+    casm: casmCode,
+    constructorCalldata: constructor,
+    salt: stark.randomAddress(),
+  });
+
+  // Connect the new contract instance :
+  const myTestContract = new Contract(
+    sierraCode.abi,
+    deployResponse.deploy.contract_address,
+    provider
+  );
+  console.log(
+    `✅ Contract has been deploy with the address: ${myTestContract.address}`
+  );
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
